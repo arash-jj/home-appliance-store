fbc5fcd9a964cbe3d1cb3bfef5cb876ea45742d9
arash-jj <arshjfry66@gmail.com>
2025-05-14 14:40:40 +0330

    NavBar-UI added

---
+++ components/NabBar.tsx
@@ -0,0 +1,44 @@
+import React from 'react'
+import Link from 'next/link'
+
+const NabBar = () => {
+    return (
+        <div className='navBarContainer'>
+            <div>
+                <Link href="/">
+                    <img src="/navbar/Furniro.svg" alt="Furniro Logo" />
+                </Link>
+            </div>
+            <div className="navBarLinksContainer">
+                <Link href="/">
+                    <p>Home</p>
+                </Link>
+                <Link href="/shop">
+                    <p>Shop</p>
+                </Link>
+                <Link href="/about">
+                    <p>About</p>
+                </Link>
+                <Link href="/contact">
+                    <p>Contact</p>
+                </Link>
+            </div>
+            <div className="featuresContainer">
+                <Link href="/">
+                    <img src="/navbar/Account.svg" alt="Account Logo" />
+                </Link>
+                <Link href="/">
+                    <img src="/navbar/Search.svg" alt="Search Logo" />
+                </Link>
+                <Link href="/">
+                    <img src="/navbar/Heart.svg" alt="Heart Logo" />
+                </Link>
+                <Link href="/">
+                    <img src="/navbar/Shopping-cart.svg" alt="Shopping-cart Logo" />
+                </Link>
+            </div>
+        </div>
+    )
+}
+
+export default NabBar
